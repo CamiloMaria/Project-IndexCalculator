d82764bb1ddf054b282af394a01868b33d9262b5
CamiloMaria <63072095+CamiloMaria@users.noreply.github.com>
2022-07-16 04:03:34 -0400

    Add files via upload

---
+++ IndexCalculator/Login/LoginHome.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LoginHome.aspx.cs" Inherits="IndexCalculator.Login.LoginHome" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="../Content/bootstrap.min.css" rel="stylesheet" />
+    <script src="../Scripts/jquery-3.0.0.min.js"></script>
+    <script src="../Scripts/popper.min.js"></script>
+    <script src="../Scripts/bootstrap.min.js"></script>
+    <link href="../Content/newLoginStyle.css" rel="stylesheet" />
+    <title>Login</title>     
+</head>
+<body>
+   <form id="form1" runat="server">
+
+    <div style="background-image:url('../Image/sign-up.png.jpg'); width:100%; height:900px; background-repeat: no-repeat; background-size:cover; background-attachment:fixed;">
+    <div class="container login-container">
+        <div class="m-0 row justify-content-center">
+                <div class="col-md-6 login-form-2">
+                    <h3>Login</h3>
+                        <div class="form-group">
+                           <asp:TextBox ID="txtID" runat="server" CssClass="form-control" placeholder="Ingrese su ID*" required></asp:TextBox>
+                        </div>
+                        <div class="form-group">
+                           <asp:TextBox ID="txtPassword" runat="server" TextMode="Password" CssClass="form-control" placeholder="Ingrese su contraseña*" required></asp:TextBox>                         
+                        </div>
+                        <div>
+                            <asp:Label runat="server" ID="lblError" CssClass="alert-danger"></asp:Label>
+                        </div>
+                        <div class="form-group">
+                            <asp:Button ID="BtnIngresar" CssClass="form-control btn btn-primary btn-dark" runat="server" Text="Ingresar" OnClick="BtnIngresar_Click"/>
+                        </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ IndexCalculator/Login/LoginHome.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace IndexCalculator.Login
+{
+    public partial class LoginHome : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void BtnIngresar_Click(object sender, EventArgs e)
+        {
+            string conectar = ConfigurationManager.ConnectionStrings["SchoolCS"].ConnectionString;
+            SqlConnection sqlConectar = new SqlConnection(conectar);
+            SqlCommand cmd = new SqlCommand("SP_ValidarAdmin", sqlConectar)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            SqlCommand cmt = new SqlCommand("SP_ValidarTeacher", sqlConectar)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            SqlCommand cms = new SqlCommand("SP_ValidarStudent", sqlConectar)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            try
+            {
+                cmd.Connection.Open();
+                cmd.Parameters.Add("@Cedula", SqlDbType.VarChar, 20).Value = txtID.Text;
+                cmd.Parameters.Add("@Password", SqlDbType.VarChar, 50).Value = txtPassword.Text;
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.Read())
+                {
+                    Session["Usuariologueado"] = txtID.Text;
+                    Response.Redirect("../Admin/AdminHome.aspx");
+                }
+                else
+                {
+                    lblError.Text = "Ingresaste erroneamente el ID o Contraseña";
+                }
+                cmd.Connection.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            try
+            {
+                cmt.Connection.Open();
+                cmt.Parameters.Add("@Cedula", SqlDbType.VarChar, 20).Value = txtID.Text;
+                cmt.Parameters.Add("@Password", SqlDbType.VarChar, 50).Value = txtPassword.Text;
+                SqlDataReader dt = cmt.ExecuteReader();
+                if (dt.Read())
+                {
+                    Session["Usuariologueado"] = txtID.Text;
+                    Response.Redirect("../Teacher/TeacherHome.aspx");
+                }
+                else
+                {
+                    lblError.Text = "Ingresaste erroneamente el ID o Contraseña";
+                }
+                cmt.Connection.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            try
+            {
+                cms.Connection.Open();
+                cms.Parameters.Add("@Cedula", SqlDbType.VarChar, 20).Value = txtID.Text;
+                cms.Parameters.Add("@Password", SqlDbType.VarChar, 50).Value = txtPassword.Text;
+                SqlDataReader ds = cms.ExecuteReader();
+
+                if (ds.Read())
+                {
+                    Session["Usuariologueado"] = txtID.Text;
+                    Response.Redirect("../Student/StudentHome.aspx");
+                }
+                else
+                {
+                    lblError.Text = "Ingresaste erroneamente el ID o Contraseña";
+                }
+                cms.Connection.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }      
+        }
+
+    }
+}
